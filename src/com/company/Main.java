0bdf38f09cce1f50e0774547d9c3ad77a4171d6e
svlasenko <svetlana.vlasenko@microfocus.com>
2021-09-28 23:10:01 +0300

    Ex2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello!!!");
+        // write your code here
+        for (int i = 0; i < 101; i++) {
+            if (i % 3 == 0) {
+            System.out.println("Hello");
+            if (i % 5 == 0)
+            System.out.println("Word");
+        }else if (i % 3 == 0 && i % 5 == 0) {
+            System.out.println("HelloWorld");
+        } else {
+                System.out.println(i);
+            }
+        }
     }
+
 }
