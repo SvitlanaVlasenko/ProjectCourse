4afb064ef0e235a78b806aae5ef245b74c30ee45
SvitlanaVlasenko <svt.vlasenko@gmail.com>
2021-10-04 23:22:56 +0300

    Add package homework2

--- src/homework2/Homework2.java
+++ src/homework2/Homework2.java
@@ -2,5 +2,10 @@ package homework2;
 
 public class Homework2 {
     public static void main(String[] args) {
+        /**You're given strings jewels representing the types of stones that are jewels,
+         and stones representing the stones you have.
+         Each character in stones is a type of stone you have.
+         You want to know how many of the stones you have are also jewels.*/
+
     }
 }
