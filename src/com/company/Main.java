3911922163c1c3b6cce7784cd6020ce59c74bb56
svlasenko <svetlana.vlasenko@microfocus.com>
2021-10-04 12:18:25 +0300

    "Home work1"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,61 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
-        // 2) Вывести ряд чисел от 1 до 100 через пробел, с изменениями:
-        //- если число кратно 3 то вместо него вывести Hello
-        //- если число кратно 5 то вместо него вывести World
-        //- если число кратно и 3 и 5 то вместо него вывести HelloWorld
+
+        System.out.println(createString());
+
+        /** 2) Вывести ряд чисел от 1 до 100 через пробел, с изменениями:
+         - если число кратно 3 то вместо него вывести Hello
+         - если число кратно 5 то вместо него вывести World
+         - если число кратно и 3 и 5 то вместо него вывести HelloWorld*/
         for (int i = 0; i < 101; i++) {
-            if (i % 3 == 0) {
-                System.out.println("Hello");
-                if (i % 5 == 0)
-                    System.out.println("Word");
-            } else if (i % 3 == 0 && i % 5 == 0) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("HelloWorld");
+            } else if (i % 3 == 0) {
+                System.out.println("Hello");
+            } else if (i % 5 == 0) {
+                System.out.println("Word");
             } else {
                 System.out.println(i);
+
             }
         }
 
-        //3) Заполнить массив из 10 элементов случайными целыми числами от 10(включительно) до 20(включительно). Вывести среднее арифметическое.
+        /**3) Заполнить массив из 10 элементов случайными целыми числами от 10(включительно) до 20(включительно).
+         Вывести среднее арифметическое.*/
         int[] array = new int[10];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (int) Math.round((Math.random() * 21));
-            System.out.println(array[i]);
+        for (int j = 0; j < array.length; j++) {
+            array[j] = (int) Math.round((Math.random() * 21) + 9);
+            double sum = 0;
+            for (j = 0; j < array.length; j++) {
+                sum += array[j];
+            }
+            sum /= array.length;
+            System.out.println("Summa=" + sum);
         }
-
-        //4) Написать метод который вернет String в которой будут в одну строку числа от 1 до 30. Каждое число при этом взять в круглые скобки ()
 
     }
 
+    /**
+     * 4) Написать метод который вернет String в которой будут в одну строку числа от 1 до 30.
+     * Каждое число при этом взять в круглые скобки ()
+     */
+    public static String createString() {
+        StringBuilder sb = new StringBuilder();
+        String result = "";
+        for (int i = 1; i <= 30; i++) {
+            result += "(" + i + ")";
+        }
+        return result;
+    }
 }
+
+
+
+
+
+
+
